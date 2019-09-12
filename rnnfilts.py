0ef547c13586451df5453b715d58c45c24d97158
dvs5680 <s.divyanshi91@gmail.com>
2019-09-12 16:39:04 -0400

    Adding classes for training CNNs and RNFs

--- rnnfilts.py
+++ rnnfilts.py
@@ -1,5 +1,4 @@
 import numpy as np
-import sys, os
 import keras
 
 # sklearn imports
@@ -8,185 +7,191 @@ from sklearn.metrics import roc_auc_score
 from sklearn.metrics import average_precision_score
 
 # keras imports
-from keras.models import Model, load_model
+from keras.callbacks import EarlyStopping
+from keras.models import Model
 from keras.layers import Dense, Dropout, Activation, Flatten, Input
-from keras.layers import Conv1D, MaxPooling1D, LSTM, Reshape, SimpleRNN
+from keras.layers import Conv1D, MaxPooling1D, LSTM, Reshape
 from keras.layers import TimeDistributed
 from keras.optimizers import SGD
 from keras.layers import Lambda
-import keras.backend as K
+
+# local imports
+from simulatedata import TrainingData, TestData
 
 
 def make_onehot(buf, seq_length):
-    fd = {'A': [1, 0, 0, 0], 'T': [0, 1, 0, 0], 'G': [0, 0, 1, 0], 'C': [0, 0, 0, 1], 'N': [0, 0, 0, 0]}
-    onehot = [fd[base] for seq in buf for base in seq]
-    onehot_np = np.reshape(onehot, (-1, seq_length, 4))
-    return onehot_np
+
+    fd = {'A': [1, 0, 0, 0], 'T': [0, 1, 0, 0],
+          'G': [0, 0, 1, 0], 'C': [0, 0, 0, 1],
+          'N': [0, 0, 0, 0]}
+
+    one_hot = [fd[base] for seq in buf for base in seq]
+    one_hot_np = np.reshape(one_hot, (-1, seq_length, 4))
+    return one_hot_np
 
 
-def embed(sequence):
-    pos = np.random.randint(5, 85)  # choose the insert position
+class ConvModel:
 
-    # Draw from a uniform 0, 100
-    choice = np.random.randint(0, 100)
-    # Random chance here, testing for transitions
-    if choice < 50:
-        sequence[pos: pos+8] = 'CAGCTGAA'
-    else:
-        sequence[pos: pos+8] = 'CAGCTGGG'
+    def __init__(self, seq_length, conv_layers, conv_filters, conv_kernel_size,
+                 dense_nodes):
+        self.seq_length = seq_length
+        self.conv_layers = conv_layers
+        self.conv_filters = conv_filters
+        self.conv_kernel_size = conv_kernel_size
+        self.dense_nodes = dense_nodes
 
+    def convolution_model(self):
+        seq_input = Input(shape=(self.seq_length, 4,), name='seq')
 
-def simulate_data():
-    # define a integer --> str dictionary
-    letter = {0: 'A', 1: 'T', 2: 'G', 3: 'C'}
-    # Simulating the negative data
-    # These are 1000 sequences with background frequencies A/T=0.5
-    seq_list = []
-    for idx in range(5000):
-        sequence = np.random.randint(0, 4, 100)
-        sequence = ''.join([letter[x] for x in sequence])
-        seq_list.append((sequence, 0))  # The 0 here is the sequence label
-    # Simulating the positive data
-    # These are 1000 sequences with an embedded motif at any position
-    for idx in range(5000):
-        sequence = np.random.randint(0, 4, 100)
-        sequence = [letter[x] for x in sequence]
-        embed(sequence)
-        seq_list.append((''.join(sequence), 1))  # Doing the join after the embedding for the positive set
-    # extracting the sequence data
-    dat = np.array(seq_list)[:, 0]
-    dat = make_onehot(dat, seq_length=100)
-    labels = np.array(seq_list)[:, 1]
-    return dat, labels
+        assert self.conv_layers > 0
 
+        for idx in range(self.conv_layers):
+            # Adding in convolution layers
+            layer_name = 'convolution' + str(idx + 1)
+            xs = Conv1D(filters=self.conv_filters, kernel_size=self.conv_kernel_size,
+                        padding='same', name=layer_name)(seq_input)
+            xs = Activation('relu')(xs)
 
-def embed_test_motif(sequence, motif):
-    pos = np.random.randint(5, 85)  # choose the insert position
-    sequence[pos: pos + 8] = motif
+        xs = MaxPooling1D(padding='same', strides=15, pool_size=15)(xs)
+        xs = Flatten()(xs)
+        # 2 FC dense layers
+        xs = Dense(self.dense_nodes, activation='relu')(xs)
+        xs = Dropout(0.5)(xs)
+        xs = Dense(self.dense_nodes, activation='relu')(xs)
+        xs = Dropout(0.5)(xs)
+        # Output
+        result = Dense(1, activation='sigmoid')(xs)
+        # Define the model input & output
+        model = Model(inputs=seq_input, outputs=result)
+        return model
 
 
-def simulate_test_dat(motif):    # define a integer --> str dictionary
-    letter = {0: 'A', 1: 'T', 2: 'G', 3: 'C'}
-    # Simulating the negative data
-    # These are 1000 sequences with background frequencies A/T=0.5
-    seq_list = []
+class RNF:
 
-    for idx in range(1000):
-        sequence = np.random.randint(0, 4, 100)
-        sequence = [letter[x] for x in sequence]
-        embed_test_motif(sequence, motif)
-        seq_list.append((''.join(sequence), 1))  # Doing the join after the embedding for the positive set
-    # extracting the sequence data
-    dat = np.array(seq_list)[:, 0]
-    dat = make_onehot(dat, seq_length=100)
-    labels = np.array(seq_list)[:, 1]
-    return dat, labels
+    def __init__(self, seq_length, rnf_filters, rnf_kernel_size, conv_filters, conv_kernel_size,
+                 dense_nodes):
+        self.seq_length = seq_length
+        self.rnf_kernel_size = rnf_kernel_size
+        self.rnf_filters = rnf_filters
+        self.conv_filters = conv_filters
+        self.conv_kernel_size = conv_kernel_size
+        self.dense_nodes = dense_nodes
 
-
-# MODEL HERE
-# Code adapted from:
-# https://github.com/keras-team/keras/issues/890
-
-def crop(dimension, start, end):
-    # Crops (or slices) a Tensor on a given dimension from start to end
-    # example : to crop tensor x[:, :, 5:10]
-    # call slice(2, 5, 10) as you want to crop on the second dimension
-    def func(x):
-        if dimension == 0:
-            return x[start: end]
-        if dimension == 1:
+    @staticmethod
+    def slice_input(start, end):
+        # Crops (or slices) a Tensor along dimension 1 from start to end
+        def func(x):
             return x[:, start: end]
-        if dimension == 2:
-            return x[:, :, start: end]
-        if dimension == 3:
-            return x[:, :, :, start: end]
-        if dimension == 4:
-            return x[:, :, :, :, start: end]
-    return Lambda(func)
+        return Lambda(func)
+
+    def rnf_model(self):
+        seq_input = Input(shape=(self.seq_length, 4,), name='seq')
+
+        # Defining RNF filters
+        def rnn_filters(idx):
+            # Start scanning here
+            start_idx = 0
+            size = self.rnf_kernel_size
+            # Define convolutional stride
+            step = 1
+
+            shared_layer = LSTM(1, name='LSTM' + str(idx))
+            # Store outputs here:
+            input_chunks = []
+            # Defining a 'valid' convolution here:
+            while start_idx + size < self.seq_length:
+                sliced_input = self.slice_input(start_idx, start_idx + size)(seq_input)
+                print sliced_input.shape
+                input_chunks.append(sliced_input)
+                start_idx += step
+
+            input_chunks = keras.layers.concatenate(input_chunks, axis=1)
+            print input_chunks.shape
+
+            dim_0_size = self.seq_length - self.rnf_kernel_size
+            input_chunks = Reshape((dim_0_size, self.rnf_kernel_size, 4))(input_chunks)
+            print input_chunks.shape
+            xs = TimeDistributed(shared_layer)(input_chunks)
+            print xs.shape
+            return xs
+
+        filter_outs = []
+        # Using the parameter for the number of filters.
+        for idx in range(self.rnf_filters):
+            filter_outs.append(rnn_filters(idx))
+
+        xs = keras.layers.concatenate(filter_outs)
+        xs = Activation('relu')(xs)
+        xs = Conv1D(filters=self.conv_filters, kernel_size=self.conv_kernel_size)(xs)
+        xs = Activation('relu')(xs)
+        # From here on, the model is identical to the convolutional model.
+        xs = MaxPooling1D(padding='same', strides=15, pool_size=15)(xs)
+        xs = Flatten()(xs)
+        # 2 FC dense layers
+        xs = Dense(self.dense_nodes, activation='relu')(xs)
+        xs = Dropout(0.5)(xs)
+        xs = Dense(self.dense_nodes, activation='relu')(xs)
+        xs = Dropout(0.5)(xs)
+        # Output
+        result = Dense(1, activation='sigmoid')(xs)
+        # Define the model input & output
+        model = Model(inputs=seq_input, outputs=result)
+        return model
 
 
-def build_model():
-    """ Define a Keras graph model with sequence and accesibility as input """
-    seq_input = Input(shape=(100, 4,), name='seq')
-    print seq_input.shape
+def fit_model(model_type, dat, labels, batch_size):
 
-    def rnn_filt(idx):
-        lstm_outs = []
-        start_idx = 0
-        size = 10
-        step = 1
-        seq_length = 90
-        shared_layer = LSTM(1, name='RNN' + str(idx))
-        input_chunks = []
-        while start_idx + step <= seq_length:
-            sliced_input = crop(1, start_idx, start_idx + size)(seq_input)
-            input_chunks.append(sliced_input)
-            start_idx += step
-        input_chunks = keras.layers.concatenate(input_chunks, axis=1)
-        print input_chunks.shape
-        input_chunks = Reshape((90, 10, 4))(input_chunks)
-        print input_chunks.shape
-        xs = TimeDistributed(shared_layer)(input_chunks)
-        print xs.shape
-        return xs
+    if model_type == 'conv':
+        architecture = ConvModel(seq_length=100, conv_layers=10, conv_filters=64,
+                                 conv_kernel_size=24, dense_nodes=32)
+        model = architecture.convolution_model()
+    else:
+        architecture = RNF(seq_length=100, rnf_filters=6, rnf_kernel_size=12,
+                           conv_filters=64, conv_kernel_size=24, dense_nodes=32)
+        model = architecture.rnf_model()
 
-    filter_outs = []
-    for idx in range(12):
-        filter_outs.append(rnn_filt(idx))
+    X_train, X_test, y_train, y_test = train_test_split(dat, labels.astype(int))
 
-    xs = keras.layers.concatenate(filter_outs)
-    xs = Activation('relu')(xs)
-
-    xs = Conv1D(filters=32, kernel_size=10)(xs) # These parameters are controlling this?
-    # xs = Conv1D(filters=64, kernel_size=16)(xs) # Are these parameters also controll
-    xs = Activation('relu')(xs)
-    xs = MaxPooling1D(pool_size=10)(xs)
-    print xs.shape
-    xs = Flatten()(xs)
-    # fully connected dense layers
-    xs = Dense(16, activation='relu')(xs)
-    result = Dense(1, activation='sigmoid')(xs)
-    # define the model input & output
-    model = Model(inputs=seq_input, outputs=result)
-    return model
-
-
-def fit_model(dat, labels):
-    model = build_model()
+    # Define the optimization here:
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
-    model.fit(x=dat, y=labels, epochs=10, batch_size=64)
-    # save the model
-    model.save('/Users/asheesh/Desktop/model.hdf5')
+    model.compile(loss='binary_crossentropy',
+                  optimizer=sgd, metrics=['accuracy'])
+    earlystop = EarlyStopping(monitor='val_loss', mode='min',
+                              verbose=1, patience=16)
+    model.fit(x=X_train, y=y_train, epochs=32, batch_size=batch_size,
+              validation_split=0.2, callbacks=[earlystop])
+
+    probas = model.predict(X_test)
+    auroc = roc_auc_score(y_test, probas)
+    auprc = average_precision_score(y_test, probas)
+    print auroc, auprc
     return model
-
-
-def evaluate_model(model, test_dat, test_labels):
-    probas = model.predict(test_dat)
-    auroc = roc_auc_score(test_labels, probas)
-    auprc = average_precision_score(test_labels, probas)
-    print "auroc:", auroc
-    print "auc(pr):", auprc
 
 
 def main():
-    dat, labels = simulate_data()
-    X_train, X_test, y_train, y_test = train_test_split(dat, labels)
-    model = fit_model(X_train, y_train.astype(int))
-    model = load_model('/Users/asheesh/Desktop/model.hdf5')
-    evaluate_model(model, X_test, y_test.astype(int))
 
-    test_dat_m, test_lab_m = simulate_test_dat('CAGCTGAA')
-    print np.mean(model.predict(test_dat_m))
+    # Defining the motifs
+    motif_a = 'TAATGA'
+    motif_b = 'TAATCT'
+    motif_flipped = 'TAATCA'
+    motif_random = 'AAAAAA'
 
-    test_dat_m, test_lab_m = simulate_test_dat('CAGCTGGG')
-    print np.mean(model.predict(test_dat_m))
+    # Instantiate an instance of TrainingData
+    # Change N / Test over many N's.
+    train_data = TrainingData(motif_a=motif_a, motif_b=motif_b, N=10000, seq_length=100)
 
-    test_dat_m, test_lab_m = simulate_test_dat('CAGCTGAG')
-    print np.mean(model.predict(test_dat_m))
+    # Getting the synthetic X and y data
+    dat, labels = train_data.simulate_data()
+    model = fit_model(model_type='conv', dat=dat, labels=labels, batch_size=64)  # Keeping the batch size fixed for now.
 
-    test_dat_m, test_lab_m = simulate_test_dat('CCCCCCCC')
-    print np.mean(model.predict(test_dat_m))
+    # Instantiating an instance of Evaluating Composition
+
+    td = TestData(seq_length=100, model=model)
+    # Testing across 4 motifs
+    print td.simulate_test_dat(motif_a)
+    print td.simulate_test_dat(motif_b)
+    print td.simulate_test_dat(motif_flipped)
+    print td.simulate_test_dat(motif_random)
 
 
 if __name__ == '__main__':
